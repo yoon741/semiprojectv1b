5f2191f8e696749510e1a74f726b214ba31a2ff6
yoon741 <mono74140@gmail.com>
2024-08-19 10:52:20 +0900

    index를 bootstrap으로 변경

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 from fastapi import FastAPI
+from starlette.requests import Request
+from starlette.responses import HTMLResponse
+from starlette.staticfiles import StaticFiles
+from starlette.templating import Jinja2Templates
 
 app = FastAPI()
+templates = Jinja2Templates(directory='views/templates')
+app.mount('/static', StaticFiles(directory='views/static'),name='static')
+
+@app.get("/", response_class=HTMLResponse)
+async def index(req: Request):
+    return templates.TemplateResponse('index.html', {'request': req})
 
 
-@app.get("/")
-async def index():
-    return {"message": "Hello World!!"}
 
 if __name__ == '__main__':
     import uvicorn
