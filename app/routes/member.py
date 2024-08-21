307445e46035491c7427b679c2b426b9e8afe97f
yoon741 <mono74140@gmail.com>
2024-08-21 09:57:04 +0900

    회원가입 완료시 member/login으로 이동

--- app/routes/member.py
+++ app/routes/member.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter
 from fastapi.params import Depends
 from sqlalchemy.orm import Session
 from starlette.requests import Request
-from starlette.responses import HTMLResponse
+from starlette.responses import HTMLResponse, RedirectResponse
 from starlette.templating import Jinja2Templates
 
 from app.dbfactory import get_db
@@ -22,6 +22,9 @@ async def joinok(member: NewMember, db: Session = Depends(get_db)):
     print(member)
     result = MemberService.insert_member(db, member)
     print('처리결과 : ', result.rowcount)
+
+    if result.rowcount > 0:     # 회원가입이 성공적으로 완료되면 url=/login로 이동
+        return RedirectResponse(url='/member/login', status_code=303)
 
 @member_router.get('/login', response_class=HTMLResponse)
 async def login(req: Request):
