bfe13f7daffc4b614354a98fbd7012fcc7b78f82
yoon741 <mono74140@gmail.com>
2024-08-21 10:48:31 +0900

    회원가입 처리시 발생할 수 있는 오류에 대한 예외처리 코드 추가

--- app/routes/member.py
+++ app/routes/member.py
@@ -19,12 +19,18 @@ async def join(req: Request):
 
 @member_router.post('/join', response_class=HTMLResponse)
 async def joinok(member: NewMember, db: Session = Depends(get_db)):
-    print(member)
-    result = MemberService.insert_member(db, member)
-    print('처리결과 : ', result.rowcount)
+    try:
+        print(member)
+        result = MemberService.insert_member(db, member)
+        print('처리결과 : ', result.rowcount)
 
-    if result.rowcount > 0:     # 회원가입이 성공적으로 완료되면 url=/login로 이동
-        return RedirectResponse(url='/member/login', status_code=303)
+        if result.rowcount > 0:     # 회원가입이 성공적으로 완료되면 url=/login로 이동
+            return RedirectResponse(url='/member/login', status_code=303)
+
+    except Exception as ex:
+        print(f'▷▷▷ joinok 오류 발생 : {str(ex)}')
+        return RedirectResponse(url='/member/error', status_code=303)
+
 
 @member_router.get('/login', response_class=HTMLResponse)
 async def login(req: Request):
@@ -33,3 +39,7 @@ async def login(req: Request):
 @member_router.get('/myinfo', response_class=HTMLResponse)
 async def myinfo(req: Request):
     return templates.TemplateResponse('member/myinfo.html', {'request': req})
+
+@member_router.get('/error', response_class=HTMLResponse)
+async def error(req: Request):
+    return templates.TemplateResponse('member/error.html', {'request': req})
--- app/service/member.py
+++ app/service/member.py
@@ -1,4 +1,5 @@
 from sqlalchemy import insert
+from sqlalchemy.exc import SQLAlchemyError
 
 from app.model.member import Member
 
@@ -6,10 +7,15 @@ from app.model.member import Member
 class MemberService:
     @staticmethod
     def insert_member(db, member):
-        stmt = insert(Member).values(
-            userid=member.userid, passwd=member.passwd,
-            name=member.name, email=member.email)
-        result = db.execute(stmt)
-        db.commit()
+        try:
+            stmt = insert(Member).values(
+                userid=member.userid, passwd=member.passwd,
+                name=member.name, email=member.email)
+            result = db.execute(stmt)
+            db.flush()
+            db.commit()  # 정상처리될 경우 commit후 return
+            return result
 
-        return result
+        except SQLAlchemyError as ex:   # 예외처리 코드
+            print(f'▶▶▶ insert_member 오류발생: {str(ex)}')
+            db.rollback()      # 정상처리되지 않을 경우 rollback
