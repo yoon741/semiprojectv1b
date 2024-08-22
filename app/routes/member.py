48f8a5c1cf4b960ed8909e9d1f0c389e715742b3
yoon741 <mono74140@gmail.com>
2024-08-22 10:15:41 +0900

    로그인 처리를 위한 loginok 작성

--- app/routes/member.py
+++ app/routes/member.py
@@ -39,6 +39,22 @@ async def joinok(member: NewMember, db: Session = Depends(get_db)):
 async def login(req: Request):
     return templates.TemplateResponse('member/login.html', {'request': req})
 
+@member_router.post('/login', response_class=HTMLResponse)
+async def loginok(req: Request, db: Session = Depends(get_db)):
+    data = await req.json()     # 클라이언트가 보낸 데이터를 request 객체로 받음
+    try:
+        redirect_url = '/member/loginfail'      # 로그인 실패시 loginfail로 이동
+
+        if MemberService.login_member(db, data):    # 로그인 성공 시
+            redirect_url = '/member/myinfo'     # myinfo 로 이동
+
+        return RedirectResponse(url=redirect_url, status_code=303)
+
+    except Exception as ex:
+        print(f'▶▶▶loginok 오류 : {str(ex)}')
+        return RedirectResponse(url='/member/error', status_code=303)
+
+
 @member_router.get('/myinfo', response_class=HTMLResponse)
 async def myinfo(req: Request):
     return templates.TemplateResponse('member/myinfo.html', {'request': req})
