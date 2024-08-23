b9aeff21bb599f4883eb08825bb3743c2dd857d6
yoon741 <mono74140@gmail.com>
2024-08-23 11:19:29 +0900

    게시판 목록 관련 라우트 및 서비스 추가

--- app/routes/board.py
+++ app/routes/board.py
@@ -1,13 +1,27 @@
 from fastapi import APIRouter, Request
-from starlette.responses import HTMLResponse
+from fastapi.params import Depends
+from sqlalchemy.orm import Session
+from starlette.responses import HTMLResponse, RedirectResponse
 from starlette.templating import Jinja2Templates
+
+from app.dbfactory import get_db
+from app.service.board import BoardService
 
 board_router = APIRouter()
 templates = Jinja2Templates(directory='views/templates')
 
 @board_router.get('/list', response_class=HTMLResponse)
-async def list(req: Request):
-    return templates.TemplateResponse('board/list.html', {'request': req})
+async def list(req: Request, db: Session = Depends(get_db)):
+    try:
+        bdlist = BoardService.select_board(db)
+        print(bdlist)
+
+        return templates.TemplateResponse('board/list.html', {'request': req})
+
+
+    except Exception as ex:
+        print(f'▶▶▶loginok 오류 발생 : {str(ex)}')
+        return RedirectResponse(url='/member/error', status_code=303)
 
 @board_router.get('/write', response_class=HTMLResponse)
 async def write(req: Request):
---
+++ app/service/board.py
@@ -0,0 +1,17 @@
+from sqlalchemy import select
+from sqlalchemy.exc import SQLAlchemyError
+
+from app.model.board import Board
+
+
+class BoardService:
+    @staticmethod
+    def select_board(db):
+        try:
+            stmt = select(Board.bno, Board.title, Board.userid,
+                          Board.regdate, Board.views).order_by(Board.bno.desc())
+            result = db.execute(stmt).scalars()
+            return result
+
+        except SQLAlchemyError as ex:
+            print(f'▸▸▸select_board 오류발생 : , {str(ex)}')
