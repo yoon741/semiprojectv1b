43c32b233b736633c82c3d708774f01d11d0f617
yoon741 <mono74140@gmail.com>
2024-08-23 14:15:33 +0900

    게시판 목록 출력에 페이지네이션 적용 2

--- app/routes/board.py
+++ app/routes/board.py
@@ -18,12 +18,27 @@ templates = Jinja2Templates(directory='views/templates')
 # ...(일종에 등가수열)
 # npage : (n - 1) * 25 + 1 ~ (n - 1) * 25 + 25
 
+# 페이지네이션 알고리즘
+# 현재페이지에 따라 보여줄 페이지 블록 결정
+# ex) 총 페이지수 : 27일때
+# => select count(bno) 총게시글수, ceil(count(bno))/25 총페이지수 from board;
+
+# cpg = 1: 1 2 3 4 5 6 7 8 9 10
+# cpg = 3: 1 2 3 4 5 6 7 8 9 10
+# cpg = 9: 1 2 3 4 5 6 7 8 9 10
+# cpg = 11: 11 12 13 14 15 16 17 18 19 20
+# cpg = 17: 11 12 13 14 15 16 17 18 19 20
+# cpg = 23: 21 22 23 24 25 26 27
+# stpgb = ((cpg - 1) / 10) * 10 + 1
+
 @board_router.get('/list/{cpg}', response_class=HTMLResponse)
 async def list(req: Request, cpg: int, db: Session = Depends(get_db)):
     try:
+        stpgb = int((cpg - 1) / 10) * 10 + 1
+
         bdlist = BoardService.select_board(db, cpg)
 
-        return templates.TemplateResponse('board/list.html', {'request': req, 'bdlist': bdlist, 'cpg': cpg})
+        return templates.TemplateResponse('board/list.html', {'request': req, 'bdlist': bdlist, 'cpg': cpg, 'stpgb': stpgb})
 
 
     except Exception as ex:
