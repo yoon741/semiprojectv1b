761f6adc38f9c8bc07c6cfa5653f895f908ee717
yoon741 <mono74140@gmail.com>
2024-08-30 13:45:00 +0900

    sqlite : regdate 마이크로 초를 0으로 설정

--- app/model/board.py
+++ app/model/board.py
@@ -24,7 +24,8 @@ class Reply(Base):
     rno: Mapped[int] = mapped_column(primary_key=True, autoincrement=True, index=True)
     reply: Mapped[str] = mapped_column(index=True)
     userid: Mapped[str] = mapped_column(ForeignKey('member.userid'), index=True)  # 비식별관계
-    regdate: Mapped[datetime] = mapped_column(default=datetime.now)
+    # regdate: Mapped[datetime] = mapped_column(default=datetime.now)
+    regdate: Mapped[datetime] = mapped_column(default=lambda: datetime.now().replace(microsecond=0))  # 초단위 수정
     bno: Mapped[int] = mapped_column(ForeignKey('board.bno'))
     rpno: Mapped[int] = mapped_column(ForeignKey('reply.rno'))
     board = relationship('Board', back_populates='replys') # 위에 board와 연결
