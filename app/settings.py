bbc2fd18c0339c655b44ac1c0dd690e16dc29d89
yoon <mono74140@gmail.com>
2024-09-25 14:22:05 +0900

    Update settings.py

--- app/settings.py
+++ app/settings.py
@@ -6,7 +6,8 @@ class Settings(BaseSettings):
     passwd: str = ''
     dbname: str = 'clouds2024'
     dburl: str = ''
-    dbconn: str = f'sqlite:///app/{dbname}.db'
+    # dbconn: str = f'sqlite:///app/{dbname}.db'
+    dbconn: str = f'mysql+pymysql://{userid}:{passwd}@{dburl}:3306/{dbname}?charset=utf8mb4'
 
 config = Settings()
 
