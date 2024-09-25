0aa1af4dfaad138066ae37d6a81abe9f31ad0b30
yoon <mono74140@gmail.com>
2024-09-25 14:55:16 +0900

    Update settings.py

--- app/settings.py
+++ app/settings.py
@@ -5,6 +5,7 @@ class Settings(BaseSettings):
     userid: str = 'ubuntu'
     passwd: str = 'ubuntu'
     dbname: str = 'clouds2024'
+    #dbname: str = 'clouds2024'
     dburl: str = 'localhost'
     dbconn: str = f'sqlite:///app/{dbname}.db'
     #dbconn: str = f'mysql+pymysql://{userid}:{passwd}@{dburl}:3306/{dbname}?charset=utf8mb4'
